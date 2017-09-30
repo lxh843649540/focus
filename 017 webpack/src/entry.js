6b82c64774bbfffde75472c7129f94fe52233ecd
lixiaohui <13554223093@163.com>
2017-09-30 14:56:44 +0800

    add webpack

---
+++ 017 webpack/src/entry.js
@@ -0,0 +1,2 @@
+import css from './css/index.css';
+document.getElementById('tit').innerHTML = "hello webpack";
---
+++ 017 webpack/webpack.config.js
@@ -0,0 +1,53 @@
+const path = require('path');
+// const uglifyjsPlugin = require('uglifyjs-webpack-plugin');
+const htmlPlugin = require('html-webpack-plugin');
+const cleanPlugin = require('clean-webpack-plugin');
+module.exports = {
+	entry: {
+		entry: './src/entry.js',
+	},
+	output: {
+		path: path.resolve(__dirname, 'dist'),
+		filename: '[name].js'
+	},
+	module: {
+		rules: [
+			{
+				test: /\.css$/,
+				use: [{
+					loader: 'style-loader'
+				},
+				{
+					loader: 'css-loader'
+				}]
+			},
+			{
+				test: /\.(jpg|png|gif|svg)$/,
+				use: [{
+					loader: 'url-loader',
+					options: {
+						limit: 400000
+					}
+				}]
+			}
+		]
+	},
+	plugins: [
+		// new uglifyjsPlugin(),
+		new cleanPlugin(['dist']),
+		new htmlPlugin ({
+			title: 'webpack',
+			minify: {
+				removeAttributeQuotes: true
+			},
+			hash: true,
+			template: './src/index.html'
+		})
+	],
+	devServer: {
+		contentBase: path.resolve(__dirname, 'dist'),
+		host: '192.168.1.120',
+		port: 1702,
+		compress: true
+	}
+};
