934aa31c76096f7768990f305f135d8f5360c5f9
李小辉 <lixh@xmazmail.com>
2017-10-25 10:40:51 +0800

    add javascript

---
+++ 007 JavaScript/Day01/day01ALL/2_node.js
@@ -0,0 +1,1 @@
+console.log("Hello Node 来自 WS");
---
+++ 007 JavaScript/Day01/text.js
@@ -0,0 +1,31 @@
+"use strict"
+// var hb="香辣鸡腿堡";
+// var yl="可乐";
+// var xch="薯条";
+//简写模式:
+// var hb="香辣鸡腿堡<br>",
+//     yl="可乐<br>",
+//     xch="薯条";
+// document.write("您的订单商品如下:<br>");
+// document.write("第一份 "+hb+"第二份 "+yl+"第三份 "+xch);
+// const PI=3.1415926;
+// var r=10;
+// var area=PI*(r*r);
+// document.write(area);
+// var height=170;
+// const FORMULA=height*2-6;
+// document.write("<br>"+FORMULA);
+// document.write("<br>"+5*2);
+// var phone="1355422";
+// //转二进制
+// console.log(r.toString(2));
+// console.log(r.toString(8));
+//汉字代号查找(八进制结果)
+// var char="李",
+//     eng="小",
+//     num="辉";
+// document.write(char.charCodeAt().toString(2),
+//                 "&nbsp;&nbsp;"+eng.charCodeAt(),
+//     "&nbsp;&nbsp;"+num.charCodeAt());
+// var meg="来自\"新华社\"的消息";
+// document.write(meg);
